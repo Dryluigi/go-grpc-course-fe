8f13009ec561ee51ee4c8dc259c05ca791486660
dryluigi <marioprasetyamulya85@gmail.com>
2025-03-10 14:08:00 +0700

    feat(change-password): integrate with API

--- src/components/ChangePasswordSection/ChangePasswordSection.tsx
+++ src/components/ChangePasswordSection/ChangePasswordSection.tsx
@@ -1,20 +1,123 @@
+import { useForm } from "react-hook-form";
+import FormInput from "../FormInput/FormInput";
+import * as yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
+import { getAuthClient } from "../../api/grpc/client";
+import Swal from "sweetalert2";
+import { useState } from "react";
+import { RpcError } from "@protobuf-ts/runtime-rpc";
+import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../../store/auth";
+
+const changePasswordSchema = yup.object().shape({
+    current_password: yup.string().required('Kata sandi saat ini wajib diisi'),
+    new_password: yup.string().required('Kata sandi baru wajib diisi').min(6, 'Kata sandi baru minimal 6 karakter'),
+    confirm_new_password: yup.string().required('Kata sandi baru wajib diisi').oneOf([yup.ref('new_password')], 'Konfirmasi kata sandi baru harus sesuai'),
+})
+
+interface ChangePasswordFormValues {
+    current_password: string;
+    new_password: string;
+    confirm_new_password: string;
+}
+
 function ChangePasswordSection() {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const navigate = useNavigate();
+    const logoutUser = useAuthStore(state => state.logout);
+    const form = useForm<ChangePasswordFormValues>({
+        resolver: yupResolver(changePasswordSchema),
+    });
+
+    const submitHandler = async (values: ChangePasswordFormValues) => {
+        try {
+            setIsLoading(true);
+            const res = await getAuthClient().changePassword({
+                newPassword: values.new_password,
+                newPasswordConfirmation: values.confirm_new_password,
+                oldPassword: values.current_password,
+            });
+
+            if (res.response.base?.isError ?? true) {
+                if (res.response.base?.message === 'Old password is not matched') {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Ganti Password Gagal',
+                        text: 'Kata sandi lama salah.'
+                    })
+                    return
+                }
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Terjadi Kesalahan',
+                    text: 'Silakan coba beberapa saat lagi.'
+                })
+                return
+            }
+
+            Swal.fire({
+                icon: 'success',
+                title: 'Ganti Password Sukses',
+            })
+
+            form.reset();
+
+            return
+        } catch (e) {
+            if (e instanceof RpcError) {
+                if (e.code === 'UNAUTHENTICATED') {
+                    logoutUser();
+                    localStorage.removeItem('access_token');
+                    Swal.fire({
+                        title: 'Sesi Telah Berakhir',
+                        text: 'Silakan login ulang kembali.',
+                        icon: 'warning',
+                    })
+                    navigate('/');
+                    return;
+                }
+            }
+
+            Swal.fire({
+                title: 'Terjadi Kesalahan',
+                text: 'Silakan coba beberapa saat lagi.',
+                icon: 'error',
+            })
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
     return (
         <div className="p-4 p-lg-5 border bg-white">
             <h2 className="h3 mb-3 text-black">Ubah Kata Sandi</h2>
-            <div className="form-group">
-                <label className="text-black" htmlFor="current_password">Kata Sandi Saat Ini</label>
-                <input type="password" className="form-control" id="current_password" />
-            </div>
-            <div className="form-group">
-                <label className="text-black" htmlFor="new_password">Kata Sandi Baru</label>
-                <input type="password" className="form-control" id="new_password" />
-            </div>
-            <div className="form-group mb-3">
-                <label className="text-black" htmlFor="confirm_password">Konfirmasi Kata Sandi Baru</label>
-                <input type="password" className="form-control" id="confirm_password" />
-            </div>
-            <button className="btn btn-primary">Perbarui Kata Sandi</button>
+            <form onSubmit={form.handleSubmit(submitHandler)}>
+                <FormInput<ChangePasswordFormValues>
+                    errors={form.formState.errors}
+                    name="current_password"
+                    register={form.register}
+                    type="password"
+                    label="Kata Sandi Saat Ini"
+                    disabled={isLoading}
+                />
+                <FormInput<ChangePasswordFormValues>
+                    errors={form.formState.errors}
+                    name="new_password"
+                    register={form.register}
+                    type="password"
+                    label="Kata Sandi Baru"
+                    disabled={isLoading}
+                />
+                <FormInput<ChangePasswordFormValues>
+                    errors={form.formState.errors}
+                    name="confirm_new_password"
+                    register={form.register}
+                    type="password"
+                    label="Konfirmasi Kata Sandi Baru"
+                    disabled={isLoading}
+                />
+                <button type="submit" className="btn btn-primary" disabled={isLoading}>Perbarui Kata Sandi</button>
+            </form>
         </div>
     )
 }
