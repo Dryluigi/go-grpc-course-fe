30d64466f7681e4a9582a3937e6db19065438cd2
dryluigi <marioprasetyamulya85@gmail.com>
2025-03-24 13:24:47 +0700

    feat(list-cart): integrate API

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,8 +1,46 @@
 import ProductHighlightSection from '../../components/ProductHighlightSection/ProductHighlightSection'
 import PlainHeroSection from '../../components/PlainHeroSection/PlainHeroSection'
 import { Link } from 'react-router-dom'
+import useGrpcApi from '../../hooks/useGrpcApi'
+import { useEffect, useState } from 'react';
+import { getCartClient } from '../../api/grpc/client';
+import { formatToIDR } from '../../utils/number';
+
+interface CartItem {
+    id: string;
+    product_name: string;
+    product_id: string;
+    product_price: number;
+    product_image_url: string;
+    quantity: number;
+    total: number;
+}
 
 function Cart() {
+    const listApi = useGrpcApi();
+    const [items, setItems] = useState<CartItem[]>([]);
+    const [totalPrice, setTotalPrice] = useState<number>(0);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await listApi.callApi(getCartClient().listCart({}));
+
+            const newItems = res.response.items.map<CartItem>(item => ({
+                id: item.cartId,
+                product_id: item.productId,
+                product_image_url: item.productImageUrl,
+                product_name: item.productName,
+                product_price: item.productPrice,
+                quantity: Number(item.quantity),
+                total: item.productPrice * Number(item.quantity),
+            }));
+            setItems(newItems);
+            setTotalPrice(newItems.reduce<number>((currentValue, item) => currentValue + item.total, 0));
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <>
             <PlainHeroSection title='Keranjang Belanja' />
@@ -10,79 +48,51 @@ function Cart() {
             <div className="untree_co-section before-footer-section">
                 <div className="container">
                     <div className="row mb-5">
-                        <form className="col-md-12" method="post">
-                            <div className="site-blocks-table">
-                                <table className="table">
-                                    <thead>
-                                        <tr>
-                                            <th className="product-thumbnail">Gambar</th>
-                                            <th className="product-name">Produk</th>
-                                            <th className="product-price">Harga</th>
-                                            <th className="product-quantity">Kuantitas</th>
-                                            <th className="product-total">Total</th>
-                                            <th className="product-remove">Hapus</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
+                        <div className="site-blocks-table">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th className="product-thumbnail">Gambar</th>
+                                        <th className="product-name">Produk</th>
+                                        <th className="product-price">Harga</th>
+                                        <th className="product-quantity">Kuantitas</th>
+                                        <th className="product-total">Total</th>
+                                        <th className="product-remove">Hapus</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {items.map(item => (
+                                        <tr key={item.id}>
                                             <td className="product-thumbnail">
-                                                <img src="images/product-1.png" alt="Image" className="img-fluid" />
+                                                <img src={item.product_image_url} alt="Image" className="img-fluid" />
                                             </td>
                                             <td className="product-name">
-                                                <h2 className="h5 text-black">Produk 1</h2>
+                                                <h2 className="h5 text-black">{item.product_name}</h2>
                                             </td>
-                                            <td>Rp735.000</td>
+                                            <td>{formatToIDR(item.product_price)}</td>
                                             <td>
                                                 <div className="input-group mb-3 d-flex align-items-center quantity-container" style={{ maxWidth: 120 }}>
                                                     <div className="input-group-prepend">
                                                         <button className="btn btn-outline-black decrease" type="button">-</button>
                                                     </div>
-                                                    <input type="text" className="form-control text-center quantity-amount" value="1" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" />
+                                                    <input type="text" className="form-control text-center quantity-amount" value={item.quantity} disabled />
                                                     <div className="input-group-append">
                                                         <button className="btn btn-outline-black increase" type="button">+</button>
                                                     </div>
                                                 </div>
-
                                             </td>
-                                            <td>Rp735.000</td>
+                                            <td>{formatToIDR(item.total)}</td>
                                             <td><a href="#" className="btn btn-black btn-sm">X</a></td>
                                         </tr>
-
-                                        <tr>
-                                            <td className="product-thumbnail">
-                                                <img src="images/product-2.png" alt="Image" className="img-fluid" />
-                                            </td>
-                                            <td className="product-name">
-                                                <h2 className="h5 text-black">Produk 2</h2>
-                                            </td>
-                                            <td>Rp735.000</td>
-                                            <td>
-                                                <div className="input-group mb-3 d-flex align-items-center quantity-container" style={{ maxWidth: 120 }}>
-                                                    <div className="input-group-prepend">
-                                                        <button className="btn btn-outline-black decrease" type="button">-</button>
-                                                    </div>
-                                                    <input type="text" className="form-control text-center quantity-amount" value="1" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" />
-                                                    <div className="input-group-append">
-                                                        <button className="btn btn-outline-black increase" type="button">+</button>
-                                                    </div>
-                                                </div>
-
-                                            </td>
-                                            <td>Rp735.000</td>
-                                            <td><a href="#" className="btn btn-black btn-sm">X</a></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </form>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
 
                     <div className="row">
                         <div className="col-md-6">
                             <div className="row mb-5">
-                                <div className="col-md-6 mb-3 mb-md-0">
-                                    <button className="btn btn-black btn-sm btn-block">Perbarui Keranjang</button>
-                                </div>
                                 <div className="col-md-6">
                                     <Link to="/shop"><button className="btn btn-outline-black btn-sm btn-block">Lanjut Belanja</button></Link>
                                 </div>
@@ -101,7 +111,7 @@ function Cart() {
                                             <span className="text-black">Subtotal</span>
                                         </div>
                                         <div className="col-md-6 text-right">
-                                            <strong className="text-black">Rp3.450.000</strong>
+                                            <strong className="text-black">{formatToIDR(totalPrice)}</strong>
                                         </div>
                                     </div>
                                     <div className="row mb-5">
@@ -109,7 +119,7 @@ function Cart() {
                                             <span className="text-black">Total</span>
                                         </div>
                                         <div className="col-md-6 text-right">
-                                            <strong className="text-black">Rp3.450.000</strong>
+                                            <strong className="text-black">{formatToIDR(totalPrice)}</strong>
                                         </div>
                                     </div>
 
